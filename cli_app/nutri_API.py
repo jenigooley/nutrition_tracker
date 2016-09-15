7e4ad1310f0b2334141b1703ea3a5d023a7d3756
Jeni G <jeni.gooley.42@gmail.com>
2016-09-15 13:43:29 -0400

    pep8 fix

--- cli_app/nutri_API.py
+++ cli_app/nutri_API.py
@@ -1,46 +1,52 @@
 import json
 import requests
 import pprint
-import data_nutrition
+import sys
 
-results = []
-refined_results = []
-food_data = {}
 
-def api_call(username, food):
+class FoodApi():
     
-    payload = {'results':'0:3',
-	    'fields':'item_name,nf_calories,nf_serving_size,nf_sugars,nf_total_fat',
-	       'appId':'7f0a2caa',
-	       'appKey':'65f3c1d9a1d3c6f7edcf2802023fe069'}
-    try:    
-	r_search = requests.get(
-	'https://api.nutritionix.com/v1_1/search/' + food,  
-	params=payload)
-    except requests.exceptions.RequestException as e:
-	print('there is a connection error.')
-	sys.exit(1)   
+    def __init__(self):
+	self.results = []
+	self.refined_results = {}
+	self.food_data = {}
 
-    r_search = r_search.json()
-    results = r_search['hits'] 
-    #show user numbered list of top 3 results
-    refined_results  = {i: item['fields']['item_name'] 
-			     for i, item in enumerate(results)} 
+    def api_call(self, username, food):
+	
+	payload = {'results':'0:3',
+		    'fields':'*',
+		   'appId':'7f0a2caa',
+		   'appKey':'65f3c1d9a1d3c6f7edcf2802023fe069'}
+	try:    
+	    r_search = requests.get(
+	    'https://api.nutritionix.com/v1_1/search/' + food,  
+	    params=payload)
+	except requests.exceptions.RequestException as e:
+	    print('there is a connection error.')
+	    sys.exit(1)   
+    
+	r_search = r_search.json() 
+	self.results = r_search['hits'] 
+	print (self.results)
+	"""show user numbered list of top 3 results"""
+	self.refined_results  = {i: item['fields']['item_name'] 
+				 for i, item in enumerate(self.results)} 
+	pprint.pprint (self.refined_results)
 
+    def add_food(self, username, item, count):
+	"""take user input and add it to user dictionary"""     
+	
+	item = int(item)
+	for i in self.results:
+	    #print(i['fields']['item_name'], refined_results[item])
+	    if i['fields']['item_name'] == self.refined_results[item]:
+		self.food_data.update(i)
 
-def add_food(username, item, count):
-    """take user input and add it to user dictionary"""     
-    for i in results:
-	if i['fields']['item_name'] == refined_results[item]:
-	    food_data.update(i)
-    food_data['serving_amount'] = count
-    food_data['username'] = username
-    food_data.update(food_data['fields'])
-    del food_data['fields']
-    add_data = data_nutrition.NutritionData()
-    add_data.add_nutrition(food_data)
-    add_data.meals(food_data)
-    return food_data
-
+	self.food_data['serving_amount'] = count
+	self.food_data.update(self.food_data['fields'])
+	del self.food_data['fields']
+	print (self.food_data)
+	return self.food_data
+	
 if __name__ == '__main__':
-    api_call()
+    FoodApi()
